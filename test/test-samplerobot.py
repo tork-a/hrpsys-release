40698d616f086f6fb38504c74286b59c2f0340c5
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-05-11 23:01:45 +0900

    Rebase from 'debian/hydro/hrpsys'

--- test/test-samplerobot.py
+++ test/test-samplerobot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 PKG = 'hrpsys'
-NAME = 'test-hostname'
+NAME = 'test-samplerobot'
 
 import imp  ## for rosbuild
 try:
@@ -23,7 +23,7 @@ import OpenHRP
 
 rtm.nsport = 2809
 
-class PA10(HrpsysConfigurator):
+class SampleRobot(HrpsysConfigurator):
     def getRTCList(self):
         return [
             ['seq', "SequencePlayer"],
@@ -38,24 +38,32 @@ class TestJointAngle(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        h = PA10()
-        h.init(robotname="PA10Controller(Robot)0")
+        h = SampleRobot()
+        h.init(robotname="SampleRobot(Robot)0")
 
     def test_set_if_find_log(self):
-        h = PA10()
+        h = SampleRobot()
         h.findComps()
         print >>sys.stderr, "log=",h.log, "log_svc=",h.log_svc
         self.assertTrue(h.log)
         self.assertTrue(h.log_svc)
 
-    def test_get_joint_angles(self):
-        h = PA10()
+    def test_load_pattern(self):
+        h = SampleRobot()
         h.findComps()
-        print >>sys.stderr,  h.getJointAngles()
-        self.assertEqual(len(h.getJointAngles()), int(9))
+        from subprocess import check_output
+        openhrp3_path = check_output(['rospack','find','openhrp3']).rstrip() # for rosbuild
+        PKG_CONFIG_PATH=""
+        if os.path.exists(os.path.join(openhrp3_path, "bin")) :
+            PKG_CONFIG_PATH='PKG_CONFIG_PATH=%s/lib/pkgconfig:$PKG_CONFIG_PATH'%(openhrp3_path)
+
+        cmd = "%s pkg-config openhrp3.1 --variable=idl_dir"%(PKG_CONFIG_PATH)
+        os.path.join(check_output(cmd, shell=True).rstrip(), "../sample/controller/SampleController/etc/Sample")
+        h.loadPattern(os.path.join(check_output(cmd, shell=True).rstrip(), "../sample/controller/SampleController/etc/Sample"), 1)
+        self.assertEqual(h.waitInterpolation(), None)
 
     def test_set_joint_angles(self):
-        h = PA10()
+        h = SampleRobot()
         h.findComps()
         self.assertTrue(h.setJointAngles(h.getJointAngles(),1))
         self.assertEqual(h.waitInterpolation(), None)
