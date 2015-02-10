2abfcad06e4a2733a219b5f4b7d0efa318deb184
Kei Okada <kei.okada@gmail.com>
2015-02-10 19:07:32 +0900

    Rebase from 'debian/hydro/hrpsys'

---
+++ rtc/CollisionDetector/CollisionDetector.h
@@ -0,0 +1,197 @@
+// -*- C++ -*-
+/*!
+ * @file  CollisionDetector.h
+ * @brief collision detector component
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef COLLISION_DETECTOR_H
+#define COLLISION_DETECTOR_H
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+#include <rtm/CorbaPort.h>
+#include <rtm/DataInPort.h>
+#include <rtm/DataOutPort.h>
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <hrpModel/Body.h>
+#include <hrpModel/ColdetLinkPair.h>
+#include <hrpModel/ModelLoaderUtil.h>
+#include "GLscene.h"
+#include "util/SDLUtil.h"
+#include "util/LogManager.h"
+#include "TimedPosture.h"
+#include "interpolator.h"
+
+#include "VclipLinkPair.h"
+#include "CollisionDetectorService_impl.h"
+
+// Service implementation headers
+// <rtc-template block="service_impl_h">
+
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+
+// </rtc-template>
+
+using namespace RTC;
+
+/**
+   \brief sample RT component which has one data input port and one data output port
+ */
+class CollisionDetector
+  : public RTC::DataFlowComponentBase
+{
+ public:
+  /**
+     \brief Constructor
+     \param manager pointer to the Manager
+  */
+  CollisionDetector(RTC::Manager* manager);
+  /**
+     \brief Destructor
+  */
+  virtual ~CollisionDetector();
+
+  // The initialize action (on CREATED->ALIVE transition)
+  // formaer rtc_init_entry()
+  virtual RTC::ReturnCode_t onInitialize();
+
+  // The finalize action (on ALIVE->END transition)
+  // formaer rtc_exiting_entry()
+  // virtual RTC::ReturnCode_t onFinalize();
+
+  // The startup action when ExecutionContext startup
+  // former rtc_starting_entry()
+  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+
+  // The shutdown action when ExecutionContext stop
+  // former rtc_stopping_entry()
+  // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+
+  // The activated action (Active state entry action)
+  // former rtc_active_entry()
+  virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+
+  // The deactivated action (Active state exit action)
+  // former rtc_active_exit()
+  virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+
+  // The execution action that is invoked periodically
+  // former rtc_active_do()
+  virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+  // The aborting action when main logic error occurred.
+  // former rtc_aborting_entry()
+  // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+
+  // The error action in ERROR state
+  // former rtc_error_do()
+  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
+
+  // The reset action that is invoked resetting
+  // This is same but different the former rtc_init_entry()
+  // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+
+  // The state update action that is invoked after onExecute() action
+  // no corresponding operation exists in OpenRTm-aist-0.2.0
+  // virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
+
+  // The action that is invoked when execution context's rate is changed
+  // no corresponding operation exists in OpenRTm-aist-0.2.0
+  // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
+
+  bool setTolerance(const char *i_link_pair_name, double i_tolerance);
+  bool getCollisionStatus(OpenHRP::CollisionDetectorService::CollisionState &state);
+
+  bool checkIsSafeTransition(void);
+  bool enable(void);
+  bool disable(void);
+
+ protected:
+  // Configuration variable declaration
+  // <rtc-template block="config_declare">
+  
+  // </rtc-template>
+
+  // DataInPort declaration
+  // <rtc-template block="inport_declare">
+  TimedDoubleSeq m_qRef;
+  InPort<TimedDoubleSeq> m_qRefIn;
+  TimedDoubleSeq m_qCurrent;
+  InPort<TimedDoubleSeq> m_qCurrentIn;
+  
+  // </rtc-template>
+
+  // DataOutPort declaration
+  // <rtc-template block="outport_declare">
+  TimedDoubleSeq m_q;
+  OutPort<TimedDoubleSeq> m_qOut;
+  
+  // </rtc-template>
+
+  // CORBA Port declaration
+  // <rtc-template block="corbaport_declare">
+  RTC::CorbaPort m_CollisionDetectorServicePort;
+  
+  // </rtc-template>
+
+  // Service declaration
+  // <rtc-template block="service_declare">
+  CollisionDetectorService_impl m_service0;
+  
+  // </rtc-template>
+
+  // Consumer declaration
+  // <rtc-template block="consumer_declare">
+
+  
+  // </rtc-template>
+  void setupVClipModel(hrp::BodyPtr i_body);
+  void setupVClipModel(hrp::Link *i_link);
+
+ private:
+  class CollisionLinkPair {
+  public:
+      CollisionLinkPair(VclipLinkPairPtr i_pair) : point0(hrp::Vector3(0,0,0)), point1(hrp::Vector3(0,0,0)), distance(0) {
+          pair = i_pair;
+      }
+      VclipLinkPairPtr pair;
+      hrp::Vector3 point0, point1;
+      double distance;
+  };
+  CollisionDetectorComponent::GLscene m_scene;
+  LogManager<TimedPosture> m_log; 
+  SDLwindow m_window;
+  GLbody *m_glbody;
+  std::vector<Vclip::Polyhedron *> m_VclipLinks;
+  bool m_use_viewer;
+  hrp::BodyPtr m_robot;
+  std::map<std::string, CollisionLinkPair *> m_pair;
+  int m_loop_for_check, m_collision_loop;
+  bool m_safe_posture;
+  int m_recover_time;
+  double m_dt;
+  int dummy;
+  //
+  double *m_recover_jointdata, *m_lastsafe_jointdata;
+  bool *m_link_collision;
+  interpolator* m_interpolator;
+  double i_dt;
+  int default_recover_time;
+  unsigned int m_debugLevel;
+  bool m_enable;
+  OpenHRP::CollisionDetectorService::CollisionState m_state;
+};
+
+
+extern "C"
+{
+  void CollisionDetectorInit(RTC::Manager* manager);
+};
+
+#endif // COLLISION_DETECTOR_H
---
+++ rtc/TorqueController/MotorTorqueController.cpp
@@ -0,0 +1,412 @@
+// -*- C++ -*-
+
+/*!
+ * @file  MotorTorqueController.cpp
+ * @brief torque controller for single motor
+ * @date  
+ *
+ * $Id$
+ */
+
+#include "MotorTorqueController.h"
+// #include "util/Hrpsys.h"
+#include <iostream>
+#include <cmath>
+#include <boost/math/special_functions/sign.hpp>
+#include <typeinfo>
+
+#define TRANSITION_TIME 2.0 // [sec]
+#define MAX_TRANSITION_COUNT (TRANSITION_TIME/m_dt)
+#define TORQUE_MARGIN 10.0 // [Nm]
+#define DEFAULT_MIN_MAX_DQ (0.17 * m_dt) // default min/max is 10[deg/sec] = 0.17[rad/sec]
+
+MotorTorqueController::MotorTorqueController()
+{
+  // default constructor: _jname = "", _ke = _tc = _dt = 0.0
+  TwoDofController::TwoDofControllerParam param;
+  param.ke = 0.0; param.tc = 0.0; param.dt = 0.0;
+  setupController(param);
+  setupControllerCommon("", param.dt);
+  setupMotorControllerMinMaxDq(0.0, 0.0);
+}
+
+MotorTorqueController::~MotorTorqueController(void)
+{
+}
+
+// for TwoDofController
+MotorTorqueController::MotorTorqueController(std::string _jname, TwoDofController::TwoDofControllerParam &_param)
+{
+  setupController(_param);
+  setupControllerCommon(_jname, _param.dt);
+  setupMotorControllerMinMaxDq(-DEFAULT_MIN_MAX_DQ, DEFAULT_MIN_MAX_DQ); 
+}
+
+void MotorTorqueController::setupController(TwoDofController::TwoDofControllerParam &_param)
+{
+  m_motor_model_type = TWO_DOF_CONTROLLER;
+  m_normalController.setupTwoDofController(_param);
+  m_emergencyController.setupTwoDofController(_param);
+}
+
+bool MotorTorqueController::updateControllerParam(TwoDofController::TwoDofControllerParam &_param)
+{
+  if (m_motor_model_type == TWO_DOF_CONTROLLER) {
+    bool retval;
+    retval = m_normalController.updateTwoDofControllerParam(_param);
+    retval = m_emergencyController.updateTwoDofControllerParam(_param) && retval;
+    return retval;
+  } else {
+    std::cerr << "motor model type is not TwoDofController" << std::endl;
+    return false;
+  }
+}
+
+// for TwoDofControllerPDModel
+MotorTorqueController::MotorTorqueController(std::string _jname, TwoDofControllerPDModel::TwoDofControllerPDModelParam &_param)
+{
+  setupController(_param);
+  setupControllerCommon(_jname, _param.dt);
+  setupMotorControllerMinMaxDq(-DEFAULT_MIN_MAX_DQ, DEFAULT_MIN_MAX_DQ);
+}
+void MotorTorqueController::setupController(TwoDofControllerPDModel::TwoDofControllerPDModelParam &_param)
+{
+  m_motor_model_type = TWO_DOF_CONTROLLER_PD_MODEL;
+  m_normalController.setupTwoDofControllerPDModel(_param);
+  m_emergencyController.setupTwoDofControllerPDModel(_param);
+}
+bool MotorTorqueController::updateControllerParam(TwoDofControllerPDModel::TwoDofControllerPDModelParam &_param)
+{
+  if (m_motor_model_type == TWO_DOF_CONTROLLER_PD_MODEL) {
+    bool retval;
+    retval = m_normalController.updateTwoDofControllerPDModelParam(_param);
+    retval = m_emergencyController.updateTwoDofControllerPDModelParam(_param) && retval;
+    return retval;
+  } else {
+    std::cerr << "motor model type is not TwoDofControllerPDModel" << std::endl;
+    return false;
+  }
+}
+
+// for TwoDofControllerDynamicsModel
+MotorTorqueController::MotorTorqueController(std::string _jname, TwoDofControllerDynamicsModel::TwoDofControllerDynamicsModelParam &_param)
+{
+  setupController(_param);
+  setupControllerCommon(_jname, _param.dt);
+  setupMotorControllerMinMaxDq(-DEFAULT_MIN_MAX_DQ, DEFAULT_MIN_MAX_DQ);
+}
+
+void MotorTorqueController::setupController(TwoDofControllerDynamicsModel::TwoDofControllerDynamicsModelParam &_param)
+{
+  m_motor_model_type = TWO_DOF_CONTROLLER_DYNAMICS_MODEL;
+  m_normalController.setupTwoDofControllerDynamicsModel(_param);
+  m_emergencyController.setupTwoDofControllerDynamicsModel(_param);
+}
+bool MotorTorqueController::updateControllerParam(TwoDofControllerDynamicsModel::TwoDofControllerDynamicsModelParam &_param)
+{
+  if (m_motor_model_type == TWO_DOF_CONTROLLER_DYNAMICS_MODEL) {
+    bool retval;
+    retval = m_normalController.updateTwoDofControllerDynamiccsModelParam(_param);
+    retval = m_emergencyController.updateTwoDofControllerDynamiccsModelParam(_param) && retval;
+    return retval;
+  } else {
+    std::cerr << "motor model type is not TwoDofControllerDynamicsModel" << std::endl;
+    return false;
+  }
+}
+
+// common public functions
+void MotorTorqueController::setupMotorControllerMinMaxDq(double _min_dq, double _max_dq)
+{
+  m_normalController.min_dq = _min_dq;
+  m_emergencyController.min_dq = _min_dq;
+  m_normalController.max_dq = _max_dq;
+  m_emergencyController.max_dq = _max_dq;
+  return;
+}
+
+bool MotorTorqueController::activate(void)
+{
+  bool retval = false;
+  if (m_normalController.state == INACTIVE) {
+    resetMotorControllerVariables(m_normalController);
+    m_normalController.controller->reset();
+    m_normalController.state = ACTIVE;
+    retval = true;
+  } else {
+    std::cerr << "[ERROR] Torque control in " << m_joint_name << " is already active" << std::endl;
+    retval = false;
+  }
+  return retval;
+}
+
+bool MotorTorqueController::deactivate(void)
+{
+  prepareStop(m_normalController);
+  return true;
+}
+
+bool MotorTorqueController::setReferenceTorque(double _tauRef)
+{
+  m_command_tauRef = _tauRef;
+  return true;
+}
+
+double MotorTorqueController::execute (double _tau, double _tauMax)
+{
+  // define controller state
+  double dq, limitedTauRef;
+ 
+  // define emergency state
+  if (std::abs(_tau) > std::abs(_tauMax)) {
+    if (m_emergencyController.state != ACTIVE) {
+      // save transtion of current controller 
+      if (m_emergencyController.state != INACTIVE) {
+        m_emergencyController.transition_dq = m_emergencyController.getMotorControllerDq();
+      } else if (m_normalController.state != INACTIVE) {
+        m_emergencyController.transition_dq = m_normalController.getMotorControllerDq();
+      }
+      m_emergencyController.dq = 0;
+      m_emergencyController.controller->reset();
+      m_emergencyController.state = ACTIVE;
+    }
+  } else {
+    if (m_emergencyController.state == ACTIVE &&
+        std::abs(_tau) <= std::max(std::abs(_tauMax) - TORQUE_MARGIN, 0.0)) {
+      if (m_normalController.state != INACTIVE) { // take control over normal process
+        m_normalController.transition_dq = m_emergencyController.getMotorControllerDq();
+        m_emergencyController.state = INACTIVE;
+      } else { // activate stop process for emergency
+        prepareStop(m_emergencyController);
+      }
+    }
+  }
+
+  // execute torque control and renew state
+  limitedTauRef = std::min(std::max(-_tauMax, m_command_tauRef), _tauMax);
+  updateController(_tau, limitedTauRef, m_normalController);
+  dq = m_normalController.getMotorControllerDq();
+  if (m_emergencyController.state != INACTIVE) { // overwrite by tauMax control when emergency mode
+    limitedTauRef = boost::math::copysign(_tauMax, _tau);
+    updateController(_tau, limitedTauRef, m_emergencyController);
+    dq = m_emergencyController.getMotorControllerDq();
+  }
+
+  // for debug
+  m_current_tau = _tau;
+  m_actual_tauRef = limitedTauRef;
+  
+  return dq;
+}
+
+std::string MotorTorqueController::getJointName(void)
+{
+  return m_joint_name;
+}
+
+MotorTorqueController::controller_state_t MotorTorqueController::getMotorControllerState(void)
+{
+  if (m_emergencyController.state == INACTIVE) {
+    return m_normalController.state;
+  } else {
+    return m_emergencyController.state;
+  }
+}
+
+void MotorTorqueController::printMotorControllerVariables(void)
+{
+  std::string prefix = "[MotorTorqueController]";
+  prefix += m_joint_name + ".";
+  std::cerr << prefix << "normalController.state:" << m_normalController.state  << std::endl;
+  std::cerr << prefix << "normalController.dq:" << m_normalController.getMotorControllerDq()  << std::endl;
+  std::cerr << prefix << "emergencyController.state:" << m_emergencyController.state  << std::endl;
+  std::cerr << prefix << "emergencyController.dq:" << m_emergencyController.getMotorControllerDq() << std::endl;
+  std::cerr << prefix << "tau:" << m_current_tau  << std::endl;
+  std::cerr << prefix << "command_tauRef:" << m_command_tauRef  << std::endl;
+  std::cerr << prefix << "actual_tauRef:" << m_actual_tauRef  << std::endl;
+  std::cerr << std::endl;
+}
+
+MotorTorqueController::motor_model_t MotorTorqueController::getMotorModelType(void)
+{
+  return m_motor_model_type;
+}
+
+// internal functions
+void MotorTorqueController::setupControllerCommon(std::string _jname, double _dt)
+{
+  m_joint_name = _jname;
+  m_dt = _dt;
+  m_command_tauRef = 0.0;
+  m_actual_tauRef = 0.0;
+  m_normalController.state = INACTIVE;
+  resetMotorControllerVariables(m_normalController);
+  m_emergencyController.state = INACTIVE;
+  resetMotorControllerVariables(m_emergencyController);
+}
+
+void MotorTorqueController::resetMotorControllerVariables(MotorTorqueController::MotorController& _mc)
+{
+  _mc.dq = 0;
+  _mc.transition_dq = 0;
+  _mc.recovery_dq = 0;
+}
+
+void MotorTorqueController::prepareStop(MotorTorqueController::MotorController &_mc)
+{
+  // angle difference to be recoverd
+  _mc.transition_dq = _mc.getMotorControllerDq();
+
+  // determine transition in 1 cycle
+  _mc.recovery_dq = std::min(std::max(_mc.transition_dq / MAX_TRANSITION_COUNT, _mc.min_dq), _mc.max_dq); // transition in 1 cycle
+  std::cerr << _mc.recovery_dq << std::endl;
+  
+  _mc.dq = 0; // dq must be reseted after recovery_dq setting(used in getMotoroControllerDq)
+  _mc.state = STOP;
+  return;
+}
+
+void MotorTorqueController::updateController(double _tau, double _tauRef, MotorTorqueController::MotorController& _mc)
+{
+  switch (_mc.state) {
+  case ACTIVE:
+    _mc.dq += _mc.controller->update(_tau, _tauRef);
+    break;
+  case STOP:
+    if (std::abs(_mc.recovery_dq) > std::abs(_mc.transition_dq)){
+        _mc.dq = 0;
+        _mc.transition_dq = 0;
+        _mc.state = INACTIVE;
+        break;
+      }
+    _mc.transition_dq -= _mc.recovery_dq;
+    break;
+  default:
+    _mc.controller->reset();
+    resetMotorControllerVariables(_mc);
+    break;
+  }
+  return;
+}
+
+// for MotorController
+MotorTorqueController::MotorController::MotorController()
+{
+  state = INACTIVE;
+  dq = 0;
+  transition_dq = 0;
+  recovery_dq = 0;
+  TwoDofController::TwoDofControllerParam param;
+  param.ke = 0.0; param.tc = 0.0; param.dt = 0.0;
+  setupTwoDofController(param);
+}
+
+MotorTorqueController::MotorController::~MotorController()
+{
+}
+
+void MotorTorqueController::MotorController::setupTwoDofController(TwoDofController::TwoDofControllerParam &_param)
+{
+  controller.reset(new TwoDofController(_param));
+  controller->reset();
+}
+
+bool MotorTorqueController::MotorController::updateTwoDofControllerParam(TwoDofController::TwoDofControllerParam &_param)
+{
+  if (typeid(*controller) != typeid(TwoDofController) || boost::dynamic_pointer_cast<TwoDofController>(controller) == NULL) {
+    std::cerr << "incorrect controller type: TwoDofController" << std::endl;
+    return false;
+  }  
+  if (state != INACTIVE) {
+    std::cerr << "controller is not inactive" << std::endl;
+    return false;
+  }
+  // update parameters which are not 0 using updateParam (parameter is not updated when _param is 0)
+  TwoDofController::TwoDofControllerParam param;
+  (boost::dynamic_pointer_cast<TwoDofController>(controller))->getParameter(param);
+  updateParam(param.ke, _param.ke);
+  updateParam(param.tc, _param.tc);
+  updateParam(param.dt, _param.dt);
+  (boost::dynamic_pointer_cast<TwoDofController>(controller))->setup(param);
+  return true;
+}
+
+void MotorTorqueController::MotorController::setupTwoDofControllerPDModel(TwoDofControllerPDModel::TwoDofControllerPDModelParam &_param)
+{
+  controller.reset(new TwoDofControllerPDModel(_param));
+  controller->reset();
+}
+
+bool MotorTorqueController::MotorController::updateTwoDofControllerPDModelParam(TwoDofControllerPDModel::TwoDofControllerPDModelParam &_param)
+{
+  if (typeid(*controller) != typeid(TwoDofControllerPDModel) || boost::dynamic_pointer_cast<TwoDofControllerPDModel>(controller) == NULL) {
+    std::cerr << "incorrect controller type: TwoDofControllerPDModel" << std::endl;
+    return false;
+  }  
+  if (state != INACTIVE) {
+    std::cerr << "controller is not inactive" << std::endl;
+    return false;
+  }
+  // update parameters which are not 0 using updateParam (parameter is not updated when _param is 0)
+  TwoDofControllerPDModel::TwoDofControllerPDModelParam param;
+  (boost::dynamic_pointer_cast<TwoDofControllerPDModel>(controller))->getParameter(param);
+  updateParam(param.ke, _param.ke);
+  updateParam(param.kd, _param.kd);
+  updateParam(param.tc, _param.tc);
+  updateParam(param.dt, _param.dt);
+  (boost::dynamic_pointer_cast<TwoDofControllerPDModel>(controller))->setup(param);
+  return true;
+}
+
+void MotorTorqueController::MotorController::setupTwoDofControllerDynamicsModel(TwoDofControllerDynamicsModel::TwoDofControllerDynamicsModelParam &_param)
+{
+  controller.reset(new TwoDofControllerDynamicsModel(_param));
+  controller->reset();
+}
+
+bool MotorTorqueController::MotorController::updateTwoDofControllerDynamiccsModelParam(TwoDofControllerDynamicsModel::TwoDofControllerDynamicsModelParam &_param)
+{
+  if (typeid(*controller) != typeid(TwoDofControllerDynamicsModel) || boost::dynamic_pointer_cast<TwoDofControllerDynamicsModel>(controller) == NULL) {
+    std::cerr << "incorrect controller type: TwoDofControllerDynamicsModel" << std::endl;
+    return false;
+  }
+  if (state != INACTIVE) {
+    std::cerr << "controller is not inactive" << std::endl;
+    return false;
+  }
+  TwoDofControllerDynamicsModel::TwoDofControllerDynamicsModelParam param;
+  (boost::dynamic_pointer_cast<TwoDofControllerDynamicsModel>(controller))->getParameter(param);
+  updateParam(param.alpha, _param.alpha);
+  updateParam(param.beta, _param.beta);
+  updateParam(param.ki, _param.ki);
+  updateParam(param.tc, _param.tc);
+  updateParam(param.dt, _param.dt);
+  (boost::dynamic_pointer_cast<TwoDofControllerDynamicsModel>(controller))->setup(param);
+  return true;
+}
+
+bool MotorTorqueController::MotorController::updateParam(double &_param, const double &_new_value)
+{
+  if (_new_value != 0) { // update parameter if given value is not 0 (new_value = 0 express holding existent parameter) 
+    _param = _new_value;
+    return true;
+  }
+  return false;
+}
+
+double MotorTorqueController::MotorController::getMotorControllerDq(void)
+{
+  double ret_dq;
+  switch(state) {
+  case ACTIVE:
+    ret_dq = dq + transition_dq; // if contorller interrupt its transition, base joint angle is not qRef, qRef + transition_dq
+    break;
+  case STOP:
+    ret_dq = transition_dq;
+    break;
+  default:
+    ret_dq = dq;
+    break;
+  }
+  return ret_dq;
+}
