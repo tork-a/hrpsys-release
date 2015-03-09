322b058be5db45690f2f75ff00259d30432f936c
Kei Okada <kei.okada@gmail.com>
2015-03-09 12:10:17 +0900

    Rebase from 'rpm/indigo/hrpsys'

--- .travis.sh
+++ .travis.sh
@@ -3,6 +3,7 @@
 set -x
 
 function error {
+    travis_time_end 31
     find ${HOME}/.ros/test_results -type f -exec echo "=== {} ===" \; -exec\
  cat {} \;
     for file in ${HOME}/.ros/log/rostest-*; do echo "=== $file ==="; cat \$\
@@ -12,10 +13,35 @@ file; done
 
 trap error ERR
 
+function travis_time_start {
+    set +x
+    TRAVIS_START_TIME=$(date +%s%N)
+    TRAVIS_TIME_ID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+    TRAVIS_FOLD_NAME=$1
+    echo -e "\e[0Ktraivs_fold:start:$TRAVIS_FOLD_NAME"
+    echo -e "\e[0Ktraivs_time:start:$TRAVIS_TIME_ID"
+    set -x
+}
+function travis_time_end {
+    set +x
+    _COLOR=${1:-32}
+    TRAVIS_END_TIME=$(date +%s%N)
+    TIME_ELAPSED_SECONDS=$(( ($TRAVIS_END_TIME - $TRAVIS_START_TIME)/1000000000 ))
+    echo -e "traivs_time:end:$TRAVIS_TIME_ID:start=$TRAVIS_START_TIME,finish=$TRAVIS_END_TIME,duration=$(($TRAVIS_END_TIME - $TRAVIS_START_TIME))\n\e[0K"
+    echo -e "traivs_fold:end:$TRAVIS_FOLD_NAME"
+    echo -e "\e[0K\e[${_COLOR}mFunction $TRAVIS_FOLD_NAME takes $(( $TIME_ELAPSED_SECONDS / 60 )) min $(( $TIME_ELAPSED_SECONDS % 60 )) sec\e[0m"
+    set -x
+}
+
+travis_time_start mongo_hack
+
 # MongoDB hack
 dpkg -s mongodb || echo "ok"; export HAVE_MONGO_DB=$?
 if [ $HAVE_MONGO_DB == 0 ]; then sudo apt-get remove -qq -y mongodb mongodb-10gen || echo "ok"; fi
 if [ $HAVE_MONGO_DB == 0 ]; then sudo apt-get install -qq -y mongodb-clients mongodb-server -o Dpkg::Options::="--force-confdef" || echo "ok"; fi # default actions
+
+travis_time_end
+travis_time_start setup_ros
 
 export CI_SOURCE_PATH=$(pwd)
 export REPOSITORY_NAME=${PWD##*/}
@@ -24,22 +50,37 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros-shadow-fixed/ubuntu precise ma
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update -qq
 
+travis_time_end
+travis_time_start disable_ssl
+
 # disable ssl
 git config --global http.sslVerify false
+
+travis_time_end
 
 case $TEST_PACKAGE in
     hrpsys)
         case $TEST_TYPE in
             iob)
+                travis_time_start  install_wget
+
                 sudo apt-get install -qq -y cproto wget
+
+                travis_time_end
+                travis_time_start  iob_test
+
                 wget https://github.com/fkanehiro/hrpsys-base/raw/315.1.9/lib/io/iob.h -O iob.h.315.1.9
                 echo -e "#define pid_t int\n#define size_t int\n#include \"lib/io/iob.h\""  | cproto -x - | sort > iob.h.current
                 echo -e "#define pid_t int\n#define size_t int\n#include \"iob.h.315.1.9\"" | cproto -x - | sort > iob.h.stable
                 cat iob.h.current
                 cat iob.h.stable
                 diff iob.h.current iob.h.stable || exit 1
+
+                travis_time_end
                 ;;
             stable_rtc)
+                travis_time_start  install_openrtm
+
                 sudo apt-get install -qq -y omniidl diffstat wget ros-hydro-openrtm-aist
                 source /opt/ros/hydro/setup.bash
                 ## check stableRTCList
@@ -51,6 +92,9 @@ case $TEST_PACKAGE in
                 if [ "`python -c "import python.hrpsys_config; hcf=python.hrpsys_config.HrpsysConfigurator(); print [ x[1] for x in hcf.getRTCList()]" | tail -1`" != "['SequencePlayer', 'StateHolder', 'ForwardKinematics', 'CollisionDetector', 'SoftErrorLimiter', 'DataLogger']" ]; then
                     exit 1
                 fi
+
+                travis_time_end
+                travis_time_start  check_idl
 
                 ## check idl
                 mkdir stable_idl
@@ -65,22 +109,41 @@ case $TEST_PACKAGE in
                     diff stable_idl/${sk_file} ${sk_file} | tee >(cat - 1>&2)  | diffstat | grep -c deletion && exit 1
                 done
                 echo "ok"
+
+                travis_time_end
                 ;;
             *)
+                travis_time_start  install_libs
+
                 # COMPILE_ONLY
                 sudo apt-get install -qq -y freeglut3-dev python-tk jython doxygen graphviz libboost-all-dev libsdl1.2-dev libglew1.6-dev libqhull-dev libirrlicht-dev libxmu-dev libcv-dev libhighgui-dev libopencv-contrib-dev
+
+                travis_time_end
+                travis_time_start  install_openhrp3
 
                 sudo apt-get install -qq -y ros-hydro-openhrp3
                 source /opt/ros/hydro/setup.bash
                 mkdir -p ~/build
+
+                travis_time_end
+                travis_time_start  compile_hrpsys
+
                 cd ~/build && cmake ${CI_SOURCE_PATH} && make
+
+                travis_time_end
                 ;;
         esac
         ;;
     *)
+        travis_time_start  install_libs
+
         # COMPILE
         sudo apt-get install -qq -y freeglut3-dev python-tk jython doxygen libboost-all-dev libsdl1.2-dev libglew1.6-dev libqhull-dev libirrlicht-dev libxmu-dev libcv-dev libhighgui-dev libopencv-contrib-dev
         # check rtmros_common
+
+        travis_time_end
+        travis_time_start  install_$TEST_PACKAGE
+
         pkg=$TEST_PACKAGE
         sudo apt-get install -qq -y python-wstool ros-hydro-catkin ros-hydro-mk ros-hydro-rostest ros-hydro-rtmbuild ros-hydro-roslint
 
@@ -88,21 +151,18 @@ case $TEST_PACKAGE in
 
         source /opt/ros/hydro/setup.bash
 
-        # set hrpsys (hrpsys wrapper for ROS, just for compile and test)
+        travis_time_end
+        travis_time_start  setup_catkin_ws
+
+        # set hrpsys in catkin workspace
         mkdir -p ~/catkin_ws/src
         cd ~/catkin_ws/src
         wstool init
-        wstool set hrpsys http://github.com/start-jsk/hrpsys --git -y
-        wstool update
-        # compile HEAD of hrpsys-base
-        sed -i s/^GIT_REVISION/#GIT_REVISION/g hrpsys/Makefile.hrpsys-base
-        sed -i s/^GIT_PATCH/#GIT_PATCH/g hrpsys/Makefile.hrpsys-base
+        ln -sf ${CI_SOURCE_PATH} hrpsys
+        sed -i "s@if(ENABLE_DOXYGEN)@if(0)@" hrpsys/CMakeLists.txt         # disable doc generation
         cd ~/catkin_ws
 
-        # us hrpsys-base what the travis donwnloaded
-        mkdir -p build/hrpsys/build
-        ln -sf ${CI_SOURCE_PATH} build/hrpsys/build/hrpsys-base-source
-        touch build/hrpsys/installed
+        travis_time_end
 
         if [ "$TEST_TYPE" == "work_with_downstream" ]; then
             echo "
@@ -110,32 +170,55 @@ case $TEST_PACKAGE in
             # check newer version of hrpsys works on current rtmros_common deb package
             # [hrpsys:new] <-> [rtmros_common:old] + [hrpsys:old]
             "
+
+            travis_time_start  compile_and_install_downstream
+
             sudo dpkg -r --force-depends ros-hydro-hrpsys
-            catkin_make -j8 -l8
-            catkin_make install -j8 -l8
-            source install/setup.bash
+
+            catkin_make_isolated --install -j1 -l1
+            # you need to pretend this is catkin package since you only have hrpsys in catkin_ws
+            export ROS_PACKAGE_PATH=`pwd`/install_isolated/share:`pwd`/install_isolated/stacks:$ROS_PACKAGE_PATH
+            source install_isolated/setup.bash
+
+            travis_time_end
         else
             echo "
             #
             # check rtmros_common compiled on newer version of hrpsys works with deb version of hrpsys
             # [hrpsys:old] <-> [rtmros_common:new] + [hrpsys:new]
             "
+
+            travis_time_start  setup_wstool_rtmros
+
+            #
+            find /opt/ros/hydro/share/hrpsys
+            sudo cp /opt/ros/hydro/bin/hrpsys-simulator /opt/ros/hydro/share/hrpsys/
+            sudo cp /opt/ros/hydro/bin/ProjectGenerator /opt/ros/hydro/share/hrpsys/
             # set up sorce code of downstream package
             cd src
             wstool set rtmros_common http://github.com/start-jsk/rtmros_common --git -y
             wstool set rtmros_hironx http://github.com/start-jsk/rtmros_hironx --git -y
             wstool set rtmros_nextage http://github.com/tork-a/rtmros_nextage --git -y
             wstool update
+            sudo apt-get install -qq -y ros-hydro-urdf
 
             cd ..
+            # do not compile unstable rtc
+            sed -i -e 's/\(add_subdirectory(\(RobotHardware\|SequencePlayer\|StateHolder\|ForwardKinematics\|CollisionDetector\|SoftErrorLimiter\|DataLogger\))\)/#\1/' -e 's/add_subdirectory/#add_subdirectory/' -e 's/##add/add/' src/hrpsys/rtc/CMakeLists.txt
+            cat  src/hrpsys/rtc/CMakeLists.txt
             # do not copile hrpsys because we wan to use them
             sed -i "1imacro(dummy_install)\nmessage(\"install(\${ARGN})\")\nendmacro()" src/hrpsys/CMakeLists.txt
             sed -i "s@install(@dummy_install(@g" src/hrpsys/CMakeLists.txt
-            sed -i "s@install(@dummy_install(@g" src/hrpsys/catkin.cmake
-            catkin_make -j8 -l8 --only-pkg-with-deps `echo $pkg | sed s/-/_/g`
-            catkin_make install -j8 -l8
-            rm -fr install/share/hrpsys ./install/lib/pkgconfig/hrpsys.pc
-            source install/setup.bash
+
+            travis_time_end
+            travis_time_start  compile_new_version
+
+            catkin_make_isolated -j1 -l1 --install --only-pkg-with-deps `echo $pkg | sed s/-/_/g`
+            rm -fr ./install_isolated/hrpsys/share/hrpsys ./install_isolated/hrpsys/lib/pkgconfig/hrpsys.pc
+            source install_isolated/setup.bash
+
+            travis_time_end
+            travis_time_start  setup_old_hrpsys
 
             # checkokut old hrpsys
             mkdir -p ~/hrpsys_ws/src
@@ -143,23 +226,68 @@ case $TEST_PACKAGE in
             wstool init .
             wstool set hrpsys http://github.com/start-jsk/hrpsys -v 315.1.9 --git -y
             wstool update
+            sed -i "s@find_package(catkin REQUIRED COMPONENTS rostest mk openrtm_aist openhrp3)@find_package(catkin REQUIRED COMPONENTS rostest mk)\nset(openrtm_aist_PREFIX /opt/ros/hydro/)\nset(openhrp3_PREFIX /opt/ros/hydro/)@"  hrpsys/catkin.cmake
+            sed -i "s@NUM_OF_CPUS = \$(shell grep -c '^processor' /proc/cpuinfo)@NUM_OF_CPUS = 1@" hrpsys/Makefile.hrpsys-base
+            # do not compile unstable rtc
+            sed -i 's@-cd $(SVN_DIR) && $(SVN_CMDLINE) up $(SVN_REVISION)@-cd $(SVN_DIR) \&\& $(SVN_CMDLINE) up $(SVN_REVISION); sed -i -e s/\(add_subdirectory(\(RobotHardware\|SequencePlayer\|StateHolder\|ForwardKinematics\|CollisionDetector\|SoftErrorLimiter\|DataLogger\))\)/#\\1/ -e s/add_subdirectory/#add_subdirectory/ -e s/##add/add/ rtc/CMakeLists.txt@' hrpsys/Makefile.hrpsys-base
+            cat hrpsys/Makefile.hrpsys-base
+
+            #git clone -b 315.1.9 http://github.com/fkanehiro/hrpsys-base hrpsys
+            #cp ~/catkin_ws/src/hrpsys/CMakeLists.txt hrpsys/
+            #cp ~/catkin_ws/src/hrpsys/package.xml hrpsys/
             cd ~/hrpsys_ws
+            ls -al src
+            ls -al src/hrpsys
 
-            catkin_make -j8 -l8
-            catkin_make install -j8 -l8
+            travis_time_end
+            travis_time_start  compile_old_hrpsys
+
+            catkin_make_isolated -j1 -l1 --merge
+
+            travis_time_end
+            travis_time_start  install_old_hrpsys
+
+            catkin_make_isolated -j1 -l1 --install
+
             # HOTFIX: https://github.com/k-okada/hrpsys-base/commit/9ce00db.diff
-            sed -i "s@\['vs@#\['vs@g" install/lib/python2.7/dist-packages/hrpsys/hrpsys_config.py
-            sed -i "s@\['afs@#\['afs@g" install/lib/python2.7/dist-packages/hrpsys/hrpsys_config.py
-            sed -i "s@\['abc@#\['abc@g" install/lib/python2.7/dist-packages/hrpsys/hrpsys_config.py
-            sed -i "s@\['st@#\['st@g" install/lib/python2.7/dist-packages/hrpsys/hrpsys_config.py
+            sed -i "s@\['vs@#\['vs@g" install_isolated/lib/python2.7/dist-packages/hrpsys/hrpsys_config.py
+            sed -i "s@\['afs@#\['afs@g" install_isolated/lib/python2.7/dist-packages/hrpsys/hrpsys_config.py
+            sed -i "s@\['abc@#\['abc@g" install_isolated/lib/python2.7/dist-packages/hrpsys/hrpsys_config.py
+            sed -i "s@\['st@#\['st@g" install_isolated/lib/python2.7/dist-packages/hrpsys/hrpsys_config.py
 
-            source install/setup.bash
+            #cp ~/catkin_ws/src/hrpsys/package.xml install_isolated/share/hrpsys/ # old hrpsys did not do this
+            source install_isolated/setup.bash
+
+            #echo $ROS_PACKAGE_PATH
+            #export ROS_PACKAGE_PATH=`pwd`/install_isolated:$ROS_PACKAGE_PATH # for hrpsys (plane cmake)
+            ls -al install_isolated/share/hrpsys
+            rospack profile
+            rospack find hrpsys
 
             cd ~/catkin_ws
+
+            travis_time_end
         fi
+
+        travis_time_start  hot_fixes
 
         rospack profile
 
+
+        # https://github.com/fkanehiro/openhrp3/issues/46
+        ls -al /opt/ros/hydro/share/openhrp3/share/OpenHRP-3.1/sample/controller/SampleController || echo "ok"
+        sudo mkdir -p /opt/ros/hydro/share/openhrp3/share/OpenHRP-3.1/sample/controller/SampleController/etc/ || echo "ok"
+        sudo wget https://raw.githubusercontent.com/fkanehiro/openhrp3/master/sample/controller/SampleController/etc/Sample.pos -O /opt/ros/hydro/share/openhrp3/share/OpenHRP-3.1/sample/controller/SampleController/etc/Sample.pos || echo "ok"
+        sudo wget https://raw.githubusercontent.com/fkanehiro/openhrp3/master/sample/controller/SampleController/etc/Sample.vel -O /opt/ros/hydro/share/openhrp3/share/OpenHRP-3.1/sample/controller/SampleController/etc/Sample.vel || echo "ok"
+        # https://github.com/start-jsk/rtmros_hironx/issues/287
+        if [ -e /opt/ros/hydro/share/hironx_ros_bridge/test/test_hironx_ros_bridge.py ]; then
+            sudo sed -i "s@test_tf_and_controller@_test_tf_and_controller@" /opt/ros/hydro/share/hironx_ros_bridge/test/test_hironx_ros_bridge.py
+        fi
+
+        travis_time_end
+        travis_time_start  run_tests
+
+        sudo /etc/init.d/omniorb4-nameserver stop || echo "stop omniserver just in case..."
         export EXIT_STATUS=0;
         pkg_path=`rospack find \`echo $pkg | sed s/-/_/g\``
         if [ "`find $pkg_path/test -iname '*.test'`" == "" ]; then
@@ -168,6 +296,10 @@ case $TEST_PACKAGE in
             find $pkg_path/test -iname "*.test" -print0 | xargs -0 -n1 rostest || export EXIT_STATUS=$?;
         fi
 
+        # for debugging
+        [ $TEST_PACKAGE == "hrpsys-ros-bridge" ] && rostest -t hrpsys_ros_bridge test-samplerobot.test
         [ $EXIT_STATUS == 0 ] || exit 1
+
+        travis_time_end
         ;;
 esac
--- rtc/CollisionDetector/CollisionDetector.cpp
+++ rtc/CollisionDetector/CollisionDetector.cpp
@@ -14,11 +14,15 @@
 #include <hrpUtil/Eigen3d.h>
 #include <hrpUtil/Eigen4d.h>
 #include <hrpCollision/ColdetModel.h>
+#ifdef USE_HRPSYSUTIL
 #include "util/GLbody.h"
 #include "util/GLutil.h"
+#endif // USE_HRPSYSUTIL
 #include "util/BVutil.h"
+#include "RobotHardwareService.hh"
 
 #include "CollisionDetector.h"
+#include "../SoftErrorLimiter/beep.h"
 
 #define deg2rad(x)	((x)*M_PI/180)
 #define rad2deg(x)      ((x)*180/M_PI)
@@ -48,26 +52,37 @@ CollisionDetector::CollisionDetector(RTC::Manager* manager)
       // <rtc-template block="initializer">
       m_qRefIn("qRef", m_qRef),
       m_qCurrentIn("qCurrent", m_qCurrent),
+      m_servoStateIn("servoStateIn", m_servoState),
       m_qOut("q", m_q),
       m_CollisionDetectorServicePort("CollisionDetectorService"),
       // </rtc-template>
       m_loop_for_check(0),
       m_collision_loop(1),
+#ifdef USE_HRPSYSUTIL
       m_glbody(NULL),
+#endif // USE_HRPSYSUTIL
       m_use_viewer(false),
       m_robot(hrp::BodyPtr()),
+#ifdef USE_HRPSYSUTIL
       m_scene(&m_log),
       m_window(&m_scene, &m_log),
+#endif // USE_HRPSYSUTIL
       m_debugLevel(0),
       m_enable(true),
+      collision_beep_count(0),
       dummy(0)
 {
     m_service0.collision(this);
+#ifdef USE_HRPSYSUTIL
     m_log.enableRingBuffer(1);
+#endif // USE_HRPSYSUTIL
+    init_beep();
+    start_beep(3136);
 }
 
 CollisionDetector::~CollisionDetector()
 {
+  quit_beep();
 }
 
 
@@ -86,6 +101,7 @@ RTC::ReturnCode_t CollisionDetector::onInitialize()
     // Set InPort buffers
     addInPort("qRef", m_qRefIn);
     addInPort("qCurrent", m_qCurrentIn);
+    addInPort("servoStateIn", m_servoStateIn);
 
     // Set OutPort buffer
     addOutPort("q", m_qOut);
@@ -101,36 +117,6 @@ RTC::ReturnCode_t CollisionDetector::onInitialize()
     // </rtc-template>
 
     //RTC::Properties& prop = getProperties();
-
-    return RTC::RTC_OK;
-}
-
-
-
-/*
-  RTC::ReturnCode_t CollisionDetector::onFinalize()
-  {
-  return RTC::RTC_OK;
-  }
-*/
-
-/*
-  RTC::ReturnCode_t CollisionDetector::onStartup(RTC::UniqueId ec_id)
-  {
-  return RTC::RTC_OK;
-  }
-*/
-
-/*
-  RTC::ReturnCode_t CollisionDetector::onShutdown(RTC::UniqueId ec_id)
-  {
-  return RTC::RTC_OK;
-  }
-*/
-
-RTC::ReturnCode_t CollisionDetector::onActivated(RTC::UniqueId ec_id)
-{
-    std::cout << m_profile.instance_name<< ": onActivated(" << ec_id << ")" << std::endl;
 
     RTC::Manager& rtcManager = RTC::Manager::instance();
     std::string nameServer = rtcManager.getConfig()["corba.nameservers"];
@@ -148,9 +134,12 @@ RTC::ReturnCode_t CollisionDetector::onActivated(RTC::UniqueId ec_id)
     if ( prop["collision_viewer"] == "true" ) {
 	m_use_viewer = true;
     }
-
+#ifdef USE_HRPSYSUTIL
     m_glbody = new GLbody();
     m_robot = hrp::BodyPtr(m_glbody);
+#else
+    m_robot = hrp::BodyPtr(new hrp::Body());
+#endif // USE_HRPSYSUTIL
     //
     OpenHRP::BodyInfo_var binfo;
     binfo = hrp::loadBodyInfo(prop["model"].c_str(),
@@ -160,12 +149,18 @@ RTC::ReturnCode_t CollisionDetector::onActivated(RTC::UniqueId ec_id)
 		  << std::endl;
 	return RTC::RTC_ERROR;
     }
+#ifdef USE_HRPSYSUTIL
     if (!loadBodyFromBodyInfo(m_robot, binfo, true, GLlinkFactory)) {
+#else
+    if (!loadBodyFromBodyInfo(m_robot, binfo, true, hrplinkFactory)) {
+#endif // USE_HRPSYSUTIL
       std::cerr << "failed to load model[" << prop["model"] << "] in "
                 << m_profile.instance_name << std::endl;
       return RTC::RTC_ERROR;
     }
+#ifdef USE_HRPSYSUTIL
     loadShapeFromBodyInfo(m_glbody, binfo);
+#endif // USE_HRPSYSUTIL
     if ( prop["collision_model"] == "AABB" ) {
         convertToAABB(m_robot);
     } else if ( prop["collision_model"] == "convex hull" ||
@@ -209,10 +204,12 @@ RTC::ReturnCode_t CollisionDetector::onActivated(RTC::UniqueId ec_id)
         coil::stringTo(m_collision_loop, prop["collision_loop"].c_str());
         std::cerr << "set collision_loop: " << m_collision_loop << std::endl;
     }
+#ifdef USE_HRPSYSUTIL
     if ( m_use_viewer ) {
       m_scene.addBody(m_robot);
       GLlink::drawMode(GLlink::DM_COLLISION);
     }
+#endif // USE_HRPSYSUTIL
 
     // setup collision state
     m_state.angle.length(m_robot->numJoints());
@@ -232,16 +229,57 @@ RTC::ReturnCode_t CollisionDetector::onActivated(RTC::UniqueId ec_id)
     for(int i=0; i<m_robot->numJoints(); i++){
       m_q.data[i] = 0;
     }
+
+    m_servoState.data.length(m_robot->numJoints());
+    for(int i = 0; i < m_robot->numJoints(); i++) {
+        m_servoState.data[i].length(1);
+        int status = 0;
+        status |= 1<< OpenHRP::RobotHardwareService::CALIB_STATE_SHIFT;
+        status |= 1<< OpenHRP::RobotHardwareService::POWER_STATE_SHIFT;
+        status |= 1<< OpenHRP::RobotHardwareService::SERVO_STATE_SHIFT;
+        status |= 0<< OpenHRP::RobotHardwareService::SERVO_ALARM_SHIFT;
+        status |= 0<< OpenHRP::RobotHardwareService::DRIVER_TEMP_SHIFT;
+        m_servoState.data[i][0] = status;
+    }
+
+    collision_beep_freq = static_cast<int>(1.0/(3.0*m_dt)); // 3 times / 1[s]
+    return RTC::RTC_OK;
+}
+
+
+
+RTC::ReturnCode_t CollisionDetector::onFinalize()
+{
+    delete[] m_recover_jointdata;
+    delete[] m_lastsafe_jointdata;
+    delete m_interpolator;
+    delete[] m_link_collision;
+    return RTC::RTC_OK;
+}
+
+/*
+  RTC::ReturnCode_t CollisionDetector::onStartup(RTC::UniqueId ec_id)
+  {
+  return RTC::RTC_OK;
+  }
+*/
+
+/*
+  RTC::ReturnCode_t CollisionDetector::onShutdown(RTC::UniqueId ec_id)
+  {
+  return RTC::RTC_OK;
+  }
+*/
+
+RTC::ReturnCode_t CollisionDetector::onActivated(RTC::UniqueId ec_id)
+{
+    std::cout << m_profile.instance_name<< ": onActivated(" << ec_id << ")" << std::endl;
     return RTC::RTC_OK;
 }
 
 RTC::ReturnCode_t CollisionDetector::onDeactivated(RTC::UniqueId ec_id)
 {
     std::cout << m_profile.instance_name<< ": onDeactivated(" << ec_id << ")" << std::endl;
-    delete[] m_recover_jointdata;
-    delete[] m_lastsafe_jointdata;
-    delete[] m_interpolator;
-    delete[] m_link_collision;
     return RTC::RTC_OK;
 }
 
@@ -250,6 +288,9 @@ RTC::ReturnCode_t CollisionDetector::onExecute(RTC::UniqueId ec_id)
 {
     static int loop = 0;
     loop++;
+    if (m_servoStateIn.isNew()) {
+        m_servoStateIn.read();
+    }
     if ( ! m_enable ) {
         if ( DEBUGP || loop % 100 == 1) {
             std::cerr << "CAUTION!! The robot is moving without checking self collision detection!!! please send enableCollisionDetection to CollisoinDetection RTC" << std::endl;
@@ -268,6 +309,7 @@ RTC::ReturnCode_t CollisionDetector::onExecute(RTC::UniqueId ec_id)
         TimedPosture tp;
 
 	assert(m_qRef.data.length() == m_robot->numJoints());
+#ifdef USE_HRPSYSUTIL
         if ( m_use_viewer ) {
           for (int i=0; i<m_glbody->numLinks(); i++){
             ((GLlink *)m_glbody->link(i))->highlight(false);
@@ -276,6 +318,11 @@ RTC::ReturnCode_t CollisionDetector::onExecute(RTC::UniqueId ec_id)
         for (int i=0; i<m_glbody->numLinks(); i++){
             m_link_collision[m_glbody->link(i)->index] = false;
         }
+#else
+        for (int i=0; i<m_robot->numLinks(); i++){
+            m_link_collision[m_robot->link(i)->index] = false;
+        }
+#endif // USE_HRPSYSUTIL
 
         //set robot model's angle for collision check(two types)
         //  1. current safe angle .. check based on qRef
@@ -324,10 +371,12 @@ RTC::ReturnCode_t CollisionDetector::onExecute(RTC::UniqueId ec_id)
                     }
                     m_link_collision[p->link(0)->index] = true;
                     m_link_collision[p->link(1)->index] = true;
+#ifdef USE_HRPSYSUTIL
                     if ( m_use_viewer ) {
                         ((GLlink *)p->link(0))->highlight(true);
                         ((GLlink *)p->link(1))->highlight(true);
                     }
+#endif // USE_HRPSYSUTIL
                 }
             }
             if ( m_safe_posture ) {
@@ -374,9 +423,8 @@ RTC::ReturnCode_t CollisionDetector::onExecute(RTC::UniqueId ec_id)
 #endif
         }
         if ( DEBUGP ) {
-          std::cerr << "check collisions for for " << m_pair.size() << " pairs in " << (tm2.sec()-tm1.sec())*1000+(tm2.usec()-tm1.usec())/1000.0 
-                    << " [msec], safe = " << m_safe_posture << ", time = " << m_recover_time << " " << m_q.data[0] << " " << m_q.data[1] 
-                    << " " << m_q.data[2] << " " << m_q.data[3] << " " << m_q.data[4] << " " << m_q.data[5] << " " << m_q.data[6] << std::endl;
+          std::cerr << "check collisions for " << m_pair.size() << " pairs in " << (tm2.sec()-tm1.sec())*1000+(tm2.usec()-tm1.usec())/1000.0 
+                    << " [msec], safe = " << m_safe_posture << ", time = " << m_recover_time*m_dt << "[s], loop = " << m_loop_for_check << "/" << m_collision_loop << std::endl;
         }
         if ( m_pair.size() == 0 && ( DEBUGP || (loop % ((int)(5/m_dt))) == 1) ) {
             std::cerr << "CAUTION!! The robot is moving without checking self collision detection!!! please define collision_pair in configuration file" << std::endl;
@@ -384,10 +432,28 @@ RTC::ReturnCode_t CollisionDetector::onExecute(RTC::UniqueId ec_id)
         //
         m_qOut.write();
 
+        // beep sound for collision alert
+        //  check servo for collision beep sound
+        bool has_servoOn = false;
+        for (int i = 0; i < m_robot->numJoints(); i++ ){
+          int servo_state = (m_servoState.data[i][0] & OpenHRP::RobotHardwareService::SERVO_STATE_MASK) >> OpenHRP::RobotHardwareService::SERVO_STATE_SHIFT;
+          has_servoOn = has_servoOn || (servo_state == 1);
+        }
+        //  beep
+        if ( !m_safe_posture && has_servoOn ) { // If collided and some joint is servoOn
+          if ( collision_beep_count % collision_beep_freq == 0 && collision_beep_count % (collision_beep_freq * 3) != 0 ) start_beep(2352, collision_beep_freq*0.7);
+          else stop_beep();
+          collision_beep_count++;
+        } else {
+          collision_beep_count = 0;
+        }
+
         if ( ++m_loop_for_check >= m_collision_loop ) m_loop_for_check = 0;
         tp.posture.resize(m_qRef.data.length());
         for (size_t i=0; i<tp.posture.size(); i++) tp.posture[i] = m_q.data[i];
+#ifdef USE_HRPSYSUTIL
         m_log.add(tp);
+#endif // USE_HRPSYSUTIL
 
         // set collisoin state
         m_state.time = tm2;
@@ -422,7 +488,9 @@ RTC::ReturnCode_t CollisionDetector::onExecute(RTC::UniqueId ec_id)
         m_state.recover_time = m_recover_time;
         m_state.loop_for_check = m_loop_for_check;
     }
+#ifdef USE_HRPSYSUTIL
     if ( m_use_viewer ) m_window.oneStep();
+#endif // USE_HRPSYSUTIL
     return RTC::RTC_OK;
 }
 
@@ -493,7 +561,9 @@ void CollisionDetector::setupVClipModel(hrp::BodyPtr i_body)
 bool CollisionDetector::checkIsSafeTransition(void)
 {
     for ( int i = 0; i < m_q.data.length(); i++ ) {
-        if (abs(m_q.data[i] - m_qRef.data[i]) > 0.017) return false;
+        // If servoOn, check too large joint angle gap. Otherwise (servoOff), neglect too large joint angle gap.
+        int servo_state = (m_servoState.data[i][0] & OpenHRP::RobotHardwareService::SERVO_STATE_MASK) >> OpenHRP::RobotHardwareService::SERVO_STATE_SHIFT; // enum SwitchStatus {SWITCH_ON, SWITCH_OFF};
+        if (servo_state == 1 && abs(m_q.data[i] - m_qRef.data[i]) > 0.017) return false;
     }
     return true;
 }
@@ -501,6 +571,7 @@ bool CollisionDetector::checkIsSafeTransition(void)
 bool CollisionDetector::enable(void)
 {
     if (m_enable){
+        std::cerr << "CollisionDetector is already enabled." << std::endl;
         return true;
     }
 
@@ -526,6 +597,7 @@ bool CollisionDetector::enable(void)
             return false;
         }
     }
+    std::cerr << "CollisionDetector is successfully enabled." << std::endl;
 
     m_safe_posture = true;
     m_recover_time = 0;
@@ -540,6 +612,7 @@ bool CollisionDetector::disable(void)
         std::cerr << "CollisionDetector cannot be disabled because of different reference joint angle" << std::endl;
         return false;
     }
+    std::cerr << "CollisionDetector is successfully disabled." << std::endl;
     m_enable = false;
     return true;
 }
@@ -559,6 +632,13 @@ void CollisionDetector::setupVClipModel(hrp::Link *i_link)
     i_vclip_model->check();
     m_VclipLinks[i_link->index] = i_vclip_model;
 }
+
+#ifndef USE_HRPSYSUTIL
+hrp::Link *hrplinkFactory()
+{
+  return new hrp::Link();
+}
+#endif // USE_HRPSYSUTIL
 
 extern "C"
 {
