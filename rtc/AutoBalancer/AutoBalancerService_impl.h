f7622081f903fc81cec617e6e0c5b2c5e71fb01a
Kei Okada <kei.okada@gmail.com>
2015-07-10 10:10:23 +0900

    Imported upstream version '315.6.0' of 'upstream'

--- rtc/AutoBalancer/AutoBalancerService_impl.h
+++ rtc/AutoBalancer/AutoBalancerService_impl.h
@@ -18,9 +18,11 @@ public:
   CORBA::Boolean goPos( CORBA::Double x,  CORBA::Double y,  CORBA::Double th);
   CORBA::Boolean goVelocity( CORBA::Double vx,  CORBA::Double vy,  CORBA::Double vth);
   CORBA::Boolean goStop();
+  CORBA::Boolean emergencyStop();
   CORBA::Boolean setFootSteps(const OpenHRP::AutoBalancerService::FootstepSequence& fs);
   CORBA::Boolean setFootStepsWithParam(const OpenHRP::AutoBalancerService::FootstepSequence& fs, const OpenHRP::AutoBalancerService::StepParamSequence& sps);
   void waitFootSteps();
+  void waitFootStepsEarly(CORBA::Double tm);
   CORBA::Boolean startAutoBalancer(const OpenHRP::AutoBalancerService::StrSequence& limbs);
   CORBA::Boolean stopAutoBalancer();
   CORBA::Boolean setGaitGeneratorParam(const OpenHRP::AutoBalancerService::GaitGeneratorParam& i_param);
@@ -28,6 +30,9 @@ public:
   CORBA::Boolean setAutoBalancerParam(const OpenHRP::AutoBalancerService::AutoBalancerParam& i_param);
   CORBA::Boolean getAutoBalancerParam(OpenHRP::AutoBalancerService::AutoBalancerParam_out i_param);
   CORBA::Boolean getFootstepParam(OpenHRP::AutoBalancerService::FootstepParam_out i_param);
+  CORBA::Boolean adjustFootSteps(const OpenHRP::AutoBalancerService::Footstep& rfootstep, const OpenHRP::AutoBalancerService::Footstep& lfootstep);
+  CORBA::Boolean getRemainingFootstepSequence(OpenHRP::AutoBalancerService::FootstepSequence_out o_footstep);
+  CORBA::Boolean releaseEmergencyStop();
   //
   //
   void autobalancer(AutoBalancer *i_autobalancer);
