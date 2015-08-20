4d15e1540a99d19e8847c873fa9fce7b881173ac
Kei Okada <kei.okada@gmail.com>
2015-08-20 19:28:13 +0900

    Rebase from 'rpm/indigo/hrpsys'

--- rtc/AutoBalancer/testPreviewController.cpp
+++ rtc/AutoBalancer/testPreviewController.cpp
@@ -14,9 +14,15 @@ struct gait_parameter /* for test */
 
 #include<cstdio>
 
-int main()
+int main(int argc, char* argv[])
 {
   /* this is c++ version example of test-preview-filter1-modified in euslib/jsk/preview.l*/
+  bool use_gnuplot = true;
+  if (argc >= 2) {
+      if ( std::string(argv[1])== "--use-gnuplot" ) {
+          use_gnuplot = (std::string(argv[2])=="true");
+      }
+  }
 
   double dt = 0.01, max_tm = 8.0;
   std::queue<hrp::Vector3> ref_zmp_list;
@@ -45,7 +51,8 @@ int main()
   bool r = true;
   size_t index = 0;
   while (r) {
-    hrp::Vector3 p, x, qdata;
+    hrp::Vector3 p, x;
+    std::vector<hrp::Vector3> qdata;
     r = df.update(p, x, qdata, ref_zmp_list.front(), qdata, !ref_zmp_list.empty());
     if (r) {
       index++;
@@ -64,18 +71,20 @@ int main()
     if (!ref_zmp_list.empty()) ref_zmp_list.pop();
   }
   fclose(fp);
+  if (use_gnuplot) {
   FILE* gp[3];
   std::string titles[2] = {"X", "Y"};
   for (size_t ii = 0; ii < 2; ii++) {
     gp[ii] = popen("gnuplot", "w");
     fprintf(gp[ii], "set title \"%s\"\n", titles[ii].c_str());
-    fprintf(gp[ii], "plot \"%s\" using 1:%d with lines title \"cart-table zmp\"\n", fname.c_str(), ( ii * 3 + 2));
-    fprintf(gp[ii], "replot \"%s\" using 1:%d with lines title \"cog\"\n", fname.c_str(), ( ii * 3 + 3));
-    fprintf(gp[ii], "replot \"%s\" using 1:%d with lines title \"refzmp\"\n", fname.c_str(), ( ii * 3 + 4));
+    fprintf(gp[ii], "plot \"%s\" using 1:%zu with lines title \"cart-table zmp\"\n", fname.c_str(), ( ii * 3 + 2));
+    fprintf(gp[ii], "replot \"%s\" using 1:%zu with lines title \"cog\"\n", fname.c_str(), ( ii * 3 + 3));
+    fprintf(gp[ii], "replot \"%s\" using 1:%zu with lines title \"refzmp\"\n", fname.c_str(), ( ii * 3 + 4));
     fflush(gp[ii]);
   }
   double tmp;
   std::cin >> tmp;
   for (size_t j = 0; j < 2; j++) pclose(gp[j]);
+  }
   return 0;
 }
