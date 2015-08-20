613e055014a9f9a8b89692d791b287558597fa56
Kei Okada <kei.okada@gmail.com>
2015-08-20 19:23:57 +0900

    Rebase from 'rpm/hydro/hrpsys'

--- sample/SampleRobot/samplerobot_stabilizer.py
+++ sample/SampleRobot/samplerobot_stabilizer.py
@@ -13,8 +13,10 @@ except:
     import socket
     import time
 
+import math
+
 def init ():
-    global hcf, initial_pose
+    global hcf, initial_pose, hrpsys_version
     hcf = HrpsysConfigurator()
     hcf.getRTCList = hcf.getRTCListUnstable
     hcf.init ("SampleRobot(Robot)0", "$(PROJECT_DIR)/../model/sample1.wrl")
@@ -22,13 +24,16 @@ def init ():
     initial_pose = [-7.779e-005,  -0.378613,  -0.000209793,  0.832038,  -0.452564,  0.000244781,  0.31129,  -0.159481,  -0.115399,  -0.636277,  0,  0,  0,  -7.77902e-005,  -0.378613,  -0.000209794,  0.832038,  -0.452564,  0.000244781,  0.31129,  0.159481,  0.115399,  -0.636277,  0,  0,  0,  0,  0,  0]
     hcf.seq_svc.setJointAngles(initial_pose, 2.0)
     hcf.seq_svc.waitInterpolation()
-    # Start AutoBalancer
-    hcf.startAutoBalancer()
-    # Remove offset
-    for sen in ["rfsensor", "lfsensor"]:
-        ofp=hcf.rmfo_svc.getForceMomentOffsetParam(sen)[1];
-        ofp.link_offset_mass=1.9;ofp.link_offset_centroid=[0.08, 0, -0.03];
-        hcf.rmfo_svc.setForceMomentOffsetParam(sen, ofp);
+    hrpsys_version = hcf.seq.ref.get_component_profile().version
+    print("hrpsys_version = %s"%hrpsys_version)
+    if hrpsys_version >= '315.5.0':
+        # Start AutoBalancer
+        hcf.startAutoBalancer()
+        # Remove offset
+        for sen in ["rfsensor", "lfsensor"]:
+            ofp=hcf.rmfo_svc.getForceMomentOffsetParam(sen)[1];
+            ofp.link_offset_mass=1.9;ofp.link_offset_centroid=[0.08, 0, -0.03];
+            hcf.rmfo_svc.setForceMomentOffsetParam(sen, ofp);
 
 def calcCOP ():
     cop_info=rtm.readDataPort(hcf.st.port("COPInfo")).data
@@ -38,35 +43,42 @@ def calcCOP ():
             [(cop_info[1]+cop_info[1+3])/(cop_info[2]+cop_info[2+3]),(cop_info[0]+cop_info[0+3])/(cop_info[2]+cop_info[2+3])]] # total ZMP
 
 def demoGetParameter():
-    print "1. getParameter"
+    print >> sys.stderr, "1. getParameter"
     stp = hcf.st_svc.getParameter()
-    print "  getParameter() => OK"
+    print >> sys.stderr, "  getParameter() => OK"
 
 def demoSetParameter():
-    print "2. setParameter"
+    print >> sys.stderr, "2. setParameter"
     stp_org = hcf.st_svc.getParameter()
-    stp = hcf.st_svc.getParameter()
     # for tpcc
-    stp.k_tpcc_p=[0.2, 0.2]
-    stp.k_tpcc_x=[4.0, 4.0]
-    stp.k_brot_p=[0.0, 0.0]
+    stp_org.k_tpcc_p=[0.2, 0.2]
+    stp_org.k_tpcc_x=[4.0, 4.0]
+    stp_org.k_brot_p=[0.0, 0.0]
     # for eefm
-    stp.eefm_leg_inside_margin=71.12*1e-3
-    stp.eefm_leg_outside_margin=71.12*1e-3
-    stp.eefm_leg_front_margin=182.0*1e-3
-    stp.eefm_leg_rear_margin=72.0*1e-3
-    stp.eefm_k1=[-1.39899,-1.39899]
-    stp.eefm_k2=[-0.386111,-0.386111]
-    stp.eefm_k3=[-0.175068,-0.175068]
-    stp.eefm_rot_damping_gain=20*1.6*10 # Stiff parameter for simulation
-    stp.eefm_pos_damping_gain=[3500*50, 3500*50, 3500*1.0*5] # Stiff parameter for simulation
-    hcf.st_svc.setParameter(stp)
+    stp_org.eefm_leg_inside_margin=71.12*1e-3
+    stp_org.eefm_leg_outside_margin=71.12*1e-3
+    stp_org.eefm_leg_front_margin=182.0*1e-3
+    stp_org.eefm_leg_rear_margin=72.0*1e-3
+    stp_org.eefm_k1=[-1.39899,-1.39899]
+    stp_org.eefm_k2=[-0.386111,-0.386111]
+    stp_org.eefm_k3=[-0.175068,-0.175068]
+    stp_org.eefm_rot_damping_gain=20*1.6*10 # Stiff parameter for simulation
+    stp_org.eefm_pos_damping_gain=[3500*50, 3500*50, 3500*1.0*5] # Stiff parameter for simulation
+    hcf.st_svc.setParameter(stp_org)
     stp = hcf.st_svc.getParameter()
-    if stp.k_tpcc_p == stp_org.k_tpcc_p and stp.k_tpcc_x == stp_org.k_tpcc_x and stp.k_brot_p == stp_org.k_brot_p:
-        print "  setParameter() => OK"
+    vcheck = stp.k_tpcc_p == stp_org.k_tpcc_p and stp.k_tpcc_x == stp_org.k_tpcc_x and stp.k_brot_p == stp_org.k_brot_p
+    if vcheck:
+        print >> sys.stderr, "  setParameter() => OK", vcheck
+    assert(vcheck)
+
+def checkActualBaseAttitude():
+    rpy = rtm.readDataPort(hcf.rh.port("WAIST")).data.orientation
+    ret = math.degrees(rpy.r) < 0.1 and math.degrees(rpy.p) < 0.1
+    print >> sys.stderr, "  actual base rpy = ", ret, "(", rpy, ")"
+    return ret
 
 def demoStartStopTPCCST ():
-    print "3. start and stop TPCC st"
+    print >> sys.stderr, "3. start and stop TPCC st"
     if hcf.pdc:
         stp = hcf.st_svc.getParameter()
         stp.st_algorithm=OpenHRP.StabilizerService.TPCC
@@ -75,13 +87,16 @@ def demoStartStopTPCCST ():
         hcf.abc_svc.goPos(0.5, 0.1, 10)
         hcf.abc_svc.waitFootSteps()
         hcf.stopStabilizer ()
-        print "  Start and Stop Stabilizer => OK"
+        ret = checkActualBaseAttitude()
+        if ret:
+            print >> sys.stderr, "  Start and Stop Stabilizer => OK"
+        assert(ret)
     else:
-        print "  This sample is neglected in High-gain mode simulation"
+        print >> sys.stderr, "  This sample is neglected in High-gain mode simulation"
 
 
 def demoStartStopEEFMQPST ():
-    print "4. start and stop EEFMQP st"
+    print >> sys.stderr, "4. start and stop EEFMQP st"
     if hcf.pdc:
         stp = hcf.st_svc.getParameter()
         stp.st_algorithm=OpenHRP.StabilizerService.EEFMQP
@@ -90,17 +105,20 @@ def demoStartStopEEFMQPST ():
         hcf.abc_svc.goPos(0.3, 0, 0)
         hcf.abc_svc.waitFootSteps()
         hcf.stopStabilizer ()
-        print "  Start and Stop Stabilizer => OK"
+        ret = checkActualBaseAttitude()
+        if ret:
+            print >> sys.stderr, "  Start and Stop Stabilizer => OK"
+        assert(ret)
     else:
-        print "  This sample is neglected in High-gain mode simulation"
+        print >> sys.stderr, "  This sample is neglected in High-gain mode simulation"
 
 def demo():
     init()
-
-    demoGetParameter()
-    demoSetParameter()
-    demoStartStopTPCCST()
-    demoStartStopEEFMQPST()
+    if hrpsys_version >= '315.5.0':
+        demoGetParameter()
+        demoSetParameter()
+        demoStartStopTPCCST()
+        demoStartStopEEFMQPST()
 
 if __name__ == '__main__':
     demo()
